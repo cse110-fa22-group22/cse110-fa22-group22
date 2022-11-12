26b7d29d93c8d4e62b29039819967e94be525585
Emaneliforp <kietchouam@gmail.com>
2022-11-11 19:18:56 -0800

    Fixed modal, adding item to list, and implemented create.js

--- source/javascript/index.js
+++ source/javascript/index.js
@@ -1,111 +1,62 @@
 import create from './shopping/create.js';
 import update from './shopping/update.js';
 import remove from './shopping/delete.js'; // delete is a keyword
-import test from './shopping/test.js';
 
-let client = {
-    shopList: [],
-    invList: {},
-};
+let shoppingList = [];
+let inventoryList = {};
+let client = {};
 
 client.shopping = {
     create: create,
     update: update,
     delete: remove,
-    test: test
 }
 
-client.shopping.test("hello");
+window.addEventListener('DOMContentLoaded', init);
 
-/*creat */ 
-//show the dialog box
-export function show_add(){
-    document.getElementById("add_item").show();
-}
-
-//input for the dialog box and close it, creat item
-export function dialog_add(){
-    //get the value from the input 
-    const name = document.getElementById("item").value; 
-    const num = document.getElementById("num_item").value; 
-    const cate = document.getElementById("cate").value; 
-
-    console.log(name,num,cate);
-
-    //check the input is null or not
-    if((name != null && name != "")&&(num != null && num != "")&&(cate != null && cate != "")){
-        // creat item
-        var ul = document.getElementById("add_list");
-        var items = ul.getElementsByTagName("li");
-        for (var i = 0; i < items.length; ++i) {
-            if(items[i].innerHTML == `Product ${i+1}`){
-                items[i].innerHTML = '';
-                //creat a check box
-                let check_box = document.createElement("input");
-                check_box.type = 'checkbox';
-                check_box.className = "check_box";
-                items[i].appendChild(check_box);
-                
-                //Create the item description
-                //add item name
-                let p_name_0 = document.createElement("span");
-                p_name_0.innerHTML  = ` | item name: `;
-                items[i].appendChild(p_name_0);
-                let p_name = document.createElement("span");
-                p_name.innerHTML  = `${name}`;
-                items[i].appendChild(p_name);
-   
-                //add item number
-                let p_num_0 = document.createElement("span");
-                p_num_0.innerHTML  = ` | item number: `;
-                items[i].appendChild(p_num_0);
-                let p_num = document.createElement("span");
-                p_num.innerHTML  = `${num}`;
-                items[i].appendChild(p_num);
-
-                //add item category
-                let p_cate_0 = document.createElement("span");
-                p_cate_0.innerHTML  = ` | category: `;
-                items[i].appendChild(p_cate_0);
-                let p_cate = document.createElement("span");
-                p_cate.innerHTML  = `${cate}`;
-                items[i].appendChild(p_cate);
-
-
-                //items[i].innerHTML = `Product ${i+1} | Name: ${name} | Number: ${num} | Category: ${cate}  `;
-
-                //creat a edit button 
-                let btn_edit = document.createElement("button");
-                btn_edit.innerHTML = "Edit";
-                btn_edit.className = "edit_Btn"
-                items[i].appendChild(btn_edit);
-
-                //creat a delete buttom
-                let btn_dele = document.createElement("button");
-                btn_dele.innerHTML = "Delete";
-                btn_dele.className = "dele_Btn"
-                items[i].appendChild(btn_dele);
-                break;
-            }
-        
+// put all eventlisteners here
+function init() {
+    window.onclick = function(event) {
+        let modal = document.getElementById("shopping_modal");
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
-        document.getElementById("add_item").close();
-    }
-    else{
-        alert("item name or number or category can't be empty");
     }
 
+    document.getElementById("shopping_add").addEventListener("click", () => {
+        let modal = document.getElementById("shopping_modal");
+        modal.style.display = "flex";
+    });
+
+    document.getElementById("shopping_cancel").addEventListener("click", hideShoppingModal);
+    document.getElementById("shopping_submit").addEventListener("click", addShoppingItem);
 }
 
-//close  the dialog 
-export function dialog_cancle(){
-    document.getElementById("add_item").close();
+
+function hideShoppingModal(){
+    document.getElementById("shopping_modal").style.display = "none";
 }
 
+function addShoppingItem(){
+    event.preventDefault();
+    //get the value from the input 
+    const name = document.getElementById("shopping_name").value; 
+    const quanity = document.getElementById("shopping_quantity").value; 
+    const category = document.getElementById("shopping_category").value; 
 
-document.getElementById("add_but").addEventListener("click", show_add);
+    if(!name || !quanity || !category){
+        return;
+    }
+    let list = document.getElementById('shopping_list');
+    list.innerHTML += `
+        <li>
+            <input type="checkbox">
+            <span>${name}</span> | <span>quantity: ${quanity}</span> | <span>${category} </span>
+            <span>X</span>
+        </li>
+    `
+    client.shopping.create(shoppingList, name, quanity, category);
 
-document.getElementById("itme_cancel").addEventListener("click", dialog_cancle);
-
-document.getElementById("itme_ok").addEventListener("click", dialog_add);
+    hideShoppingModal();
+}
 
--- source/javascript/shopping/create.js
+++ source/javascript/shopping/create.js
@@ -1,4 +1,4 @@
-export default function(shopping, name, quantity, category) {
-    shopping.push({name: name, quantity: quantity, category: category});
-    // add to localStorage
+export default function(shoppingList, name, quantity, category) {
+    shoppingList.push({name: name, quantity: quantity, category: category});
+    localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
 }
