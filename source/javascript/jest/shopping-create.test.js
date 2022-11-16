3bde2af7c4aa856246dcb09d11d84e2dc7c3f8ef
Emaneliforp <kietchouam@gmail.com>
2022-11-16 15:43:20 -0800

    Commented out example unit test

--- source/javascript/jest/shopping-create.test.js
+++ source/javascript/jest/shopping-create.test.js
@@ -1,13 +1,13 @@
-const create = require('../shopping/create.js')
+// const create = require('../shopping/create.js')
 
-test('create item in shopping', () => {
-  const shopping = []
+// test('create item in shopping', () => {
+//   const shopping = []
 
-  create(shopping, 'apple', '1', 'fruit')
+//   create(shopping, 'apple', '1', 'fruit')
 
-  expect(shopping.length).toBe(1)
-  expect(shopping[0].name).toBe('apple')
-  expect(shopping[0].quantity).toBe('1')
-  expect(shopping[0].category).toBe('fruit')
+//   expect(shopping.length).toBe(1)
+//   expect(shopping[0].name).toBe('apple')
+//   expect(shopping[0].quantity).toBe('1')
+//   expect(shopping[0].category).toBe('fruit')
   
-})
+// })
