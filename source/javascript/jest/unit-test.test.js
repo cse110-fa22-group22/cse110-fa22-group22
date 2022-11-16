aaff53b189e14eda582d13f6bfc2c1e55d620664
Emaneliforp <kietchouam@gmail.com>
2022-11-16 15:53:13 -0800

    Fixed unit test

--- source/javascript/jest/unit-test.test.js
+++ source/javascript/jest/unit-test.test.js
@@ -1,4 +1,4 @@
-import sum from './shopping/test.js'
+const sum = require('./shopping/test.js');
 
 test('Testing sum', () => {
     expect(sum(2, 3)).toBe(5)
--- source/javascript/shopping/test.js
+++ source/javascript/shopping/test.js
@@ -1,3 +1,4 @@
-export default function (a, b) {
-    return a + b
+function sum(a, b) {
+    return a + b;
 }
+module.exports = sum;
