324bb448f38e37c9c7b5cbf5513723bfccd60578
Emaneliforp <kietchouam@gmail.com>
2022-11-15 23:57:40 -0800

    Added Jest and created a unit test folder

---
+++ source/javascript/jest/shopping-create-test.js
@@ -0,0 +1,12 @@
+const create = require('../shopping/create.js')
+
+test('create item in shopping', () => {
+  const shopping = []
+
+  create(shopping, 'apple', '1', 'fruit')
+
+  expect(shopping.length).toBe(1)
+  expect(shopping[0].name).toBe('apple')
+  expect(shopping[0].quantity).toBe('1')
+  expect(shopping[0].category).toBe('fruit')
+})
