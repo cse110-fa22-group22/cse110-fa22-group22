98323cde814c2da355aed792f4373c15d8fefa40
Emaneliforp <kietchouam@gmail.com>
2022-11-16 15:35:17 -0800

    changed ESLint indent rule to 4 spaces and comment out js files

--- source/.eslintrc.js
+++ source/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
+    "indent": ["error", 4],
   },
   ignorePatterns: ['javascript/jest']
 }
--- source/javascript/shopping/create.js
+++ source/javascript/shopping/create.js
@@ -1,4 +1,4 @@
-export default function(shopping, name, quantity, category) {
-    shopping.push({name: name, quantity: quantity, category: category});
-    // add to localStorage
-}
+// export default function (shopping, name, quantity, category) {
+//     shopping.push({name: name, quantity: quantity, category: category})
+//     // add to localStorage
+// }
