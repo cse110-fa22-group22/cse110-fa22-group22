acd49a63e85d157a9fad8c849e515d89339ae6ca
Yahoo <yac033@ucsd.edu>
2022-11-18 23:22:02 -0800

    Fix test method

--- source/javascript/jest/update-test.test.js
+++ source/javascript/jest/update-test.test.js
@@ -2,6 +2,6 @@ const update = require('../shopping/update.js');
 
 test('Testing update', () => {
     let shoppingList = [{"name":"Orange","quantity":"5","category":"fruit"},{"name":"Apple","quantity":"15","category":"fruit"}]
-    update(shoppingList, "Apple", "10", "fruit")
+    client.shopping.create(shoppingList, "Apple", "10", "fruit")
     expect(shoppingList).toBe({"name":"Orange","quantity":"5","category":"fruit"},{"name":"Apple","quantity":"10","category":"fruit"})
 })
