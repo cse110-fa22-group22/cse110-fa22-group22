794fc3bee3cda726514704f0b2adb17066d4090c
Emaneliforp <kietchouam@gmail.com>
2022-11-18 22:59:04 -0800

    Commented out unused variables

--- source/javascript/index.js
+++ source/javascript/index.js
@@ -2,8 +2,8 @@ import create from './shopping/create.js'
 import update from './shopping/update.js'
 import remove from './shopping/delete.js' // delete is a keyword
 
-const shoppingList = []
-const inventoryList = {}
+// const shoppingList = []
+// const inventoryList = {}
 const client = {}
 
 client.shopping = {
