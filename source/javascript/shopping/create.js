dc7be00558ff2bfda5b98320f21493269216c3b6
Emaneliforp <kietchouam@gmail.com>
2022-11-19 11:56:52 -0800

    Fixed create method to reject duplicate item and added more unit tests

--- source/javascript/shopping/create.js
+++ source/javascript/shopping/create.js
@@ -1,4 +1,11 @@
 export default function (shoppingList, name, quantity, category) {
+    for (let i = 0; i < shoppingList.length; i++) {
+        if (shoppingList[i].name === name) {
+            return false
+        }
+    }
+
     shoppingList.push({ name, quantity, category })
     localStorage.setItem('shoppingList', JSON.stringify(shoppingList))
+    return true
 }
