5f670a37ee21ff1228cf289cb377ddda63ca3b44
Yahoo <yac033@ucsd.edu>
2022-11-18 23:14:26 -0800

    Fix style

--- source/javascript/shopping/update.js
+++ source/javascript/shopping/update.js
@@ -1,6 +1,6 @@
 export default function(shoppingList, name, quantity, category) {
-    for(item of shoppingList){ //Looping object in shoppingList and assign it to item
-        if(item.name == name){ //If item's name is match with the name that user want to update
+    for(let item of shoppingList){ //Looping object in shoppingList and assign it to item
+        if(item.name === name){ //If item's name is match with the name that user want to update
             item.quantity = quantity//Chnage the quantity base on user's request
             item.category = category;//Chnage the category base on user's request
         }
