6c5a49897708b2f253c5117727ef51a84691158c
Yahoo <yac033@ucsd.edu>
2022-11-18 23:45:02 -0800

    Fix the testing for update method

--- source/javascript/jest/update-test.test.js
+++ source/javascript/jest/update-test.test.js
@@ -1,7 +1,29 @@
-const update = require('../shopping/update.js');
+import update from '../shopping/update.js'
+class LocalStorageMock {
+    constructor() {
+        this.store = {};
+    }
 
+    clear() {
+        this.store = {};
+    }
+
+    getItem(key) {
+        return this.store[key] || null;
+    }
+
+    setItem(key, value) {
+        this.store[key] = String(value);
+    }
+
+    removeItem(key) {
+        delete this.store[key];
+    }
+}
+  
+global.localStorage = new LocalStorageMock;
 test('Testing update', () => {
     let shoppingList = [{"name":"Orange","quantity":"5","category":"fruit"},{"name":"Apple","quantity":"15","category":"fruit"}]
-    client.shopping.create(shoppingList, "Apple", "10", "fruit")
-    expect(shoppingList).toBe({"name":"Orange","quantity":"5","category":"fruit"},{"name":"Apple","quantity":"10","category":"fruit"})
+    update(shoppingList, "Apple", "Milk", "10", "Drink")
+    expect(shoppingList).toStrictEqual([{"name":"Orange","quantity":"5","category":"fruit"},{"name":"Milk","quantity":"10","category":"Drink"}])
 })
