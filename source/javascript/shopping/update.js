78c2dc258ceac9d28c7531d43d1fbcfdb8ef6387
Yahoo <yac033@ucsd.edu>
2022-11-18 23:44:48 -0800

    Add the prevName arugment

--- source/javascript/shopping/update.js
+++ source/javascript/shopping/update.js
@@ -1,6 +1,7 @@
-export default function(shoppingList, name, quantity, category) {
+export default function(shoppingList, prevName, name, quantity, category) {
     for(let item of shoppingList){ //Looping object in shoppingList and assign it to item
-        if(item.name === name){ //If item's name is match with the name that user want to update
+        if(item.name === prevName){ //If item's name is match with the name that user want to update
+            item.name = name;//Change name
             item.quantity = quantity//Chnage the quantity base on user's request
             item.category = category;//Chnage the category base on user's request
         }
