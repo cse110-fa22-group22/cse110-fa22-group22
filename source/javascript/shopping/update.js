b84e3c3aaf97e26388efab24c71f34d789ded763
Yahoo <yac033@ucsd.edu>
2022-11-18 23:10:38 -0800

    Adding comment to create function

--- source/javascript/shopping/update.js
+++ source/javascript/shopping/update.js
@@ -1,9 +1,9 @@
 export default function(shoppingList, name, quantity, category) {
-    for(item of shoppingList){
-        if(item.name == name){
-            item.quantity = quantity
-            item.category = category;
+    for(item of shoppingList){ //Looping object in shoppingList and assign it to item
+        if(item.name == name){ //If item's name is match with the name that user want to update
+            item.quantity = quantity//Chnage the quantity base on user's request
+            item.category = category;//Chnage the category base on user's request
         }
     }
-    localStorage.setItem('shoppingList', JSON.stringify(shoppingList))
+    localStorage.setItem('shoppingList', JSON.stringify(shoppingList))//push the shoppingList back to localStorage
 }
