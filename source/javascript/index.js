8c360cd8dac2bc5b1e75e9f15c29b7fda6bafe71
Emaneliforp <kietchouam@gmail.com>
2022-11-19 16:20:58 -0800

    Added template

--- source/javascript/index.js
+++ source/javascript/index.js
@@ -30,13 +30,6 @@ function init () {
 
     document.getElementById('shopping_cancel').addEventListener('click', hideShoppingModal)
     document.getElementById('shopping_submit').addEventListener('click', addShoppingItem)
-    readItemFromStorage()
-    document.querySelectorAll('.update').forEach((update) => {
-        update.addEventListener('click', (event) => {
-            document.getElementById('shopping_modal').style.display = 'flex'
-            console.log(event.target.parentNode.parentNode.getElementsByTagName('span')[0].textContent)
-        })
-    })
 }
 
 function hideShoppingModal () {
@@ -76,35 +69,36 @@ function addShoppingItem () {
 }
 
 function addEvents () {
+    // const updateButtons = document.getElementsByClassName('update')
+    // const updateButton = updateButtons[last]
+
+    // addEventListener pass in updateButton
+
     const removeButtons = document.getElementsByClassName('remove-button')
     const removeButton = removeButtons[removeButtons.length - 1]
 
     removeButton.addEventListener('click', () => { removeShoppingItem(removeButton) })
 }
 
+/*
+update function(button) {
+    const item = button.parentNode
+    const name = item.innerHTML.split('>')[2].split('<')[0]
+
+    client.shopping.update(shoppingList, prevName, name, quanyity, category)
+
+    item.innerHTML = `<input type="checkbox">
+              <span class="name">${name}</span> |
+              <span class="quantity">quantity: ${quanity}</span> |
+              <span class="category">${category} </span>
+              <span><button class="update">update</button></span>
+              <span class="remove-button">X</span>`
+}
+*/
+
 function removeShoppingItem (button) {
     const item = button.parentNode
     const name = item.innerHTML.split('>')[2].split('<')[0]
     item.parentNode.removeChild(item)
     // delete(shoppingList, name)
-}
-
-function readItemFromStorage () {
-    const test = JSON.parse(localStorage.getItem('shoppingList'))
-    let name
-    let quanity
-    let category
-    const list = document.getElementById('shopping_list')
-    if (test != null) {
-        for (const item of test) {
-            list.innerHTML += `
-  <li>
-      <input type="checkbox">
-      <span>${item.name}</span> | <span>quantity: ${item.quantity}</span> | <span>${item.category} </span><span><button class = 'update'>update</button></span>
-      <span>X</span>
-  </li>
-  `
-            shoppingList.push({ name: item.name, quantity: item.quantity, category: item.category })
-        }
-    }
 }
