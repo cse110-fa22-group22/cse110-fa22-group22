11a7c84c80e776ace8e9e35bf8f43da724ba5a06
Yahoo <yac033@ucsd.edu>
2022-11-20 14:57:12 -0800

    uncomment the shoppingList

--- source/javascript/index.js
+++ source/javascript/index.js
@@ -2,7 +2,7 @@ import create from './shopping/create.js'
 import update from './shopping/update.js'
 import remove from './shopping/delete.js' // delete is a keyword
 
-// const shoppingList = []
+const shoppingList = []
 // const inventoryList = {}
 const client = {}
 
