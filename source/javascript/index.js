498d5ba8b026881deff11b2fe5887b1b1f1dce00
Yahoo <yac033@ucsd.edu>
2022-11-20 12:50:44 -0800

    Moving the read method into the index.js

--- source/javascript/index.js
+++ source/javascript/index.js
@@ -2,7 +2,7 @@ import create from './shopping/create.js'
 import update from './shopping/update.js'
 import remove from './shopping/delete.js' // delete is a keyword
 
-// const shoppingList = []
+const shoppingList = []
 // const inventoryList = {}
 const client = {}
 
@@ -10,4 +10,21 @@ client.shopping = {
     create,
     update,
     delete: remove
+}
+
+function readItemFromStorage() {
+    const shoppingListFromStorage = JSON.parse(localStorage.getItem('shoppingList'))
+    const list = document.getElementById('shopping_list')
+    if (shoppingListFromStorage != null) {
+        for (const item of shoppingListFromStorage) {
+            list.innerHTML += `
+    <li>
+        <input type="checkbox">
+        <span>${item.name}</span> | <span>quantity: ${item.quantity}</span> | <span>${item.category} </span><span><button class = 'update'>update</button></span>
+        <span>X</span>
+    </li>
+    `
+            shoppingList.push({ name: item.name, quantity: item.quantity, category: item.category })
+        }
+    }
 }
