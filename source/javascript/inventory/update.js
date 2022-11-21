be714f7cd6cee0cfff8024c5385e24975ff28026
Yahoo <yac033@ucsd.edu>
2022-11-20 22:12:39 -0800

    remove commented code

--- source/javascript/inventory/update.js
+++ source/javascript/inventory/update.js
@@ -2,7 +2,6 @@ import create from '../inventory/create.js'
 
 export default function (inventoryList, prevName, prevCategory, vname, quantity, category) {
     const inventoryCategory = inventoryList[prevCategory]
-    let copyInventoryList = JSON.stringify(inventoryList)
     let itemRemoved = []
     for (const item of inventoryCategory) {
         if (item.name !== prevName) {
@@ -12,10 +11,6 @@ export default function (inventoryList, prevName, prevCategory, vname, quantity,
     inventoryList[prevCategory] = itemRemoved
     if (itemRemoved.length === 0) {
         delete inventoryList[prevCategory]
-        // copyInventoryList = JSON.stringify(inventoryList)
-        // const copyInventoryListPart1 = copyInventoryList.substring(0, copyInventoryList.indexOf(prevCategory) - 1)
-        // const copyInventoryListPart2 = copyInventoryList.substring(copyInventoryList.indexOf('[],') + 3)
-        // inventoryList = JSON.parse(copyInventoryListPart1 + copyInventoryListPart2)
     }
     const name = vname
     create(inventoryList, name, quantity, category)
