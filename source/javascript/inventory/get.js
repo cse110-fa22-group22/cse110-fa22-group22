89c04df763add174f7f392de32b67a5d24d0059d
Emaneliforp <kietchouam@gmail.com>
2022-11-29 23:10:59 -0800

    Made get method not case sensitive

--- source/javascript/inventory/get.js
+++ source/javascript/inventory/get.js
@@ -2,7 +2,7 @@ export default function (inventoryList, name, category) {
     if (!inventoryList[category]) { return false }
     const arr = inventoryList[category]
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i].name === name) {
+        if (arr[i].name.tolowerCase() === name.tolowerCase()) {
             return true
         }
     }
