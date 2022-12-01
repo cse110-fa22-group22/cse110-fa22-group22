62cbe44a88e9e914d042f7f57bae1314f248609a
Terodhai <Subashkatel@users.noreply.github.com>
2022-12-01 15:46:40 -0800

    fixed the merge conflict

--- source/javascript/index.js
+++ source/javascript/index.js
@@ -6,19 +6,6 @@ import inventoryUpdate from './inventory/update.js'
 import inventoryGet from './inventory/get.js'
 import inventoryDelete from './inventory/delete.js'
 
-//menu bar hamburger closing and opening
-const hamburger = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-menu');
-/*
-Menu bar closing and opening 
-*/
-hamburger.addEventListener('click', function() {
-	hamburger.classList.toggle('is-active');
-	mobile_menu.classList.toggle('is-active');
-})
-
-
-
 const shoppingList = []
 let inventoryList = {}
 const client = {
