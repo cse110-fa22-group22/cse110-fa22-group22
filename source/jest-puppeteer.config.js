dba7351c5812095c2520fe4f97cf4c4f1c50f87e
Emaneliforp <kietchouam@gmail.com>
2022-12-01 18:59:53 -0800

    Removed puppeteer from ci

--- source/jest-puppeteer.config.js
+++ source/jest-puppeteer.config.js
@@ -1,12 +1,6 @@
 module.exports = {
     launch: {
         headless: true,
-        args: [
-            '--ignore-certificate-errors',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-accelerated-2d-canvas',
-            '--disable-gpu'
-        ]
+        slowMo: 25
     }
 }
