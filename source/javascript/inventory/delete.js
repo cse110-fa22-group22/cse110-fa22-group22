ebeff13c4841eb77019c9dbf7ab880dc21267e2f
Stephen Wilcox <stephenawilcox@gmail.com>
2022-12-02 14:01:03 -0800

    worked on delete e2e tests and delete

--- source/javascript/inventory/delete.js
+++ source/javascript/inventory/delete.js
@@ -1,8 +1,15 @@
+/**
+ * Deletes the item matching the name and category from inventoryList and updates localStorage
+ * @param {*} inventoryList list of all the categories and items in the categories
+ * @param {*} name the name of the item that is to be deleted
+ * @param {*} category the category of the item
+ * @returns true if the item is deleted and false otherwise
+ */
 export default function (inventoryList, name, category) {
     const cat = inventoryList[category]
     if (cat === undefined) { return false }
     for (let i = 0; i < cat.length; i++) {
-        if (name === cat[i].name) {
+        if (name.toLowerCase() === cat[i].name.toLowerCase()) {
             cat.splice(i, 1)
             if (cat.length === 0) {
                 delete inventoryList[category]
